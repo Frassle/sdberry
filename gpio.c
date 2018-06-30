5cabd274b13ddf981c3ca86f63e2afbd2dc285fc
Fraser Waters <frassle@gmail.com>
2018-06-30 14:52:39 +0000

    Things are kinda broken :(

--- gpio.c
+++ gpio.c
@@ -187,25 +187,15 @@ void pullUpDnControl (int pin, int pud) {
 // ============================
 
 // Card status register
-struct {
-	uint32_t OUT_OF_RANGE : 1;
-	uint32_t ADDRESS_ERROR : 1;
-	uint32_t BLOCK_LEN_ERROR : 1;
-	uint32_t ERASE_SEQ_ERROR : 1;
-	uint32_t ERASE_PARAM : 1;
-	uint32_t WP_VIOLATION : 1;
-	uint32_t CARD_IS_LOCKED : 1;
-	uint32_t LOCK_UNLOCK_FAILED : 1;
-       	uint32_t COM_CRC_ERROR : 1;
-	uint32_t ILLEGAL_COMMAND : 1;
-	uint32_t CARD_ECC_FAILED : 1;
-	uint32_t CC_ERROR : 1;
-	uint32_t reserved1 : 1;
-	uint32_t reserved2 : 1;
-	uint32_t CSD_OVERWRITE : 1;
-	uint32_t WP_ERASE_SKIP : 1;
-	uint32_t CARD_ECC_DISABLED : 1;
-	uint32_t ERASE_RESET : 1;
+typedef struct CSR_WORD {
+	uint32_t reserved6 : 2;
+	uint32_t reserved5 : 1;
+	uint32_t AKE_SEQ_ERROR : 1;
+	uint32_t reserved4 : 1;
+	uint32_t APP_CMD : 1;
+	uint32_t FX_EVENT : 1;
+	uint32_t reserved3 : 1;
+	uint32_t READY_FOR_DATA : 1;
 	// 0 == idle
 	// 1 == ready
 	// 2 == ident
@@ -217,15 +207,42 @@ struct {
 	// 8 == dis
 	// 9-15 == reserved
 	uint32_t CURRENT_STATE : 4;
-	uint32_t READY_FOR_DATA : 1;
-	uint32_t reserved3 : 1;
-	uint32_t FX_EVENT : 1;
-	uint32_t APP_CMD : 1;
-	uint32_t reserved4 : 1;
-	uint32_t AKE_SEQ_ERROR : 1;
-	uint32_t reserved5 : 1;
-	uint32_t reserved6 : 2;
-} CSR;
+	uint32_t ERASE_RESET : 1;
+	uint32_t CARD_ECC_DISABLED : 1;
+	uint32_t WP_ERASE_SKIP : 1;
+	uint32_t CSD_OVERWRITE : 1;
+	uint32_t reserved2 : 1;
+	uint32_t reserved1 : 1;
+	uint32_t CC_ERROR : 1;
+	uint32_t CARD_ECC_FAILED : 1;
+	uint32_t ILLEGAL_COMMAND : 1;
+	uint32_t COM_CRC_ERROR : 1;
+	uint32_t LOCK_UNLOCK_FAILED : 1;
+	uint32_t CARD_IS_LOCKED : 1;
+	uint32_t WP_VIOLATION : 1;
+	uint32_t ERASE_PARAM : 1;
+	uint32_t ERASE_SEQ_ERROR : 1;
+	uint32_t BLOCK_LEN_ERROR : 1;
+	uint32_t ADDRESS_ERROR : 1;
+	uint32_t OUT_OF_RANGE : 1;
+} CSR_WORD;
+
+CSR_WORD CSR;
+
+// Card IDentification register
+struct {
+	// Low word
+	uint64_t not_used : 1;
+	uint64_t CRC : 7;
+	uint64_t MDT : 12;
+	uint64_t reserved : 4;
+	uint64_t PSN : 32;
+	uint64_t PRV : 8;
+	// High word
+	uint64_t PNM : 40;
+	uint64_t OID : 16;
+	uint64_t MID : 8;
+} CID;
 
 uint8_t CRCTable[256];
  
@@ -257,6 +274,14 @@ uint8_t crc7(uint64_t word) {
 	return crc;
 }
 
+uint8_t crc7_bytes(uint8_t* bytes, uint8_t count) {
+	uint8_t crc = 0;
+	do {
+		crc = CRCTable[(crc << 1) ^ bytes[--count]];
+	} while (count > 0);
+	return crc;
+}
+
 int checkcrc(uint64_t word) {
 	uint8_t crc = (word & 0xFF) >> 1;
 	uint64_t message = word >> 8;
@@ -265,18 +290,50 @@ int checkcrc(uint64_t word) {
 		printf("Expected crc %x, got crc %x!\n", crc, real_crc);
 		printf("%#llx\n", word);
 		CSR.COM_CRC_ERROR = 1;
+		return 0;
 	} else {
-		CSR.COM_CRC_ERROR = 0;
+		return 1;
 	}
 }
 
-uint64_t send_r(uint64_t response) {
+void setupCID() {
+	memset(&CID, sizeof(CID), 0);
+	CID.not_used = 1;
+	uint8_t crc = crc7_bytes(((uint8_t*)&CID) + 1, 15);
+	CID.CRC = crc;
+}
+
+void setupCSR() {
+	memset(&CSR, sizeof(CSR), 0);
+}
+
+uint64_t wait_clocks(int wait) {
 	pinMode(20, OUTPUT);
+	digitalWrite(20, 1);
 
 	uint64_t hz = 0;
 	int high = 0;
-	int bits = 0;
-	while(bits != 48) { 
+	while(wait > 0) { 
+		int gpioreg = *(gpio + 13);
+		int clk = (gpioreg & (1 << 21)) != 0;
+
+		if(!high && clk) { 
+			++hz;
+			high = 1;
+			--wait;
+		} else if (high && !clk) {
+			high = 0;
+		}
+	}
+	return hz;
+}
+
+
+uint64_t send_r(uint64_t response, uint64_t bits) {
+	uint64_t hz = 0;
+	uint64_t mask = (1 << (bits - 1));
+	int high = 1;
+	while(bits > 0) { 
 		int gpioreg = *(gpio + 13);
 		int clk = (gpioreg & (1 << 21)) != 0;
 
@@ -285,36 +342,63 @@ uint64_t send_r(uint64_t response) {
 			high = 1;
 		} else if (high && !clk) {
 			high = 0;
-			int bit = (response & 0x800000000000) != 0;
-			response <<= 1;
-			++bits;
+			int bit = (response & mask) != 0;
 			digitalWrite(20, bit);
+			response <<= 1;
+			--bits;
 		}
 	}
-	pinMode(20, INPUT);
 	return hz;
 }
 
 uint64_t send_r1(int cmdindex) {
+	uint64_t hz = 0; //wait_clocks(2);
+
 	uint64_t response = (uint64_t)cmdindex << 32;
 	response |= *((uint32_t*)&CSR);
 	uint8_t crc = crc7(response);
 	response <<= 8;
 	response |= (crc << 1);
 	response |= 1;
-	uint64_t hz = send_r(response);
+	hz += send_r(response, 48);
 	printf("Sent R1 (%#llx)\n", response);
 	return hz;
 }
 
-uint64_t send_r3() {
-	uint64_t r3 = 0x3F40300000FF;
-	uint64_t hz = send_r(r3);
-	printf("Sent R3\n");
+uint64_t send_r2(uint64_t *reg) {
+	uint64_t hz = 0; //wait_clocks(5);
+
+	// 00111111 = 0x3F
+	hz += send_r(0x3F, 8);
+
+	uint64_t response = *(reg + 1);
+	hz += send_r(response, 64);
+	response = *reg;
+	hz += send_r(response, 64);
+	printf("Sent R2 (%#llx%llx)\n", *(reg + 1), *reg);
+	return hz;
+}
+
+uint64_t send_r3(int ccs, int busy) {
+	uint64_t hz = 0; //wait_clocks(2);
+
+	uint64_t ocr = (1 << 20) | (1 << 21); // set 3.2-3.4V
+	if (ccs) {
+		ocr |= (1 << 30);
+	}
+	if (busy) {
+		ocr |= (1 << 31);
+	}
+	// 001111110000000000000000000000000000000011111111 == 0x3F00000000FF
+	uint64_t response = 0x3F00000000FF | (ocr << 8);
+	hz += send_r(response, 48);
+	printf("Sent R3 (%#llx)\n", response);
 	return hz;
 }
 
 uint64_t send_r7(uint32_t voltage, uint32_t pattern) {
+	uint64_t hz = 0; //wait_clocks(2);
+
 	// 1001000000000000000000000000000 == 0x48000000
 	uint64_t response = 0x48000000;
 	response |= (voltage << 8);
@@ -323,7 +407,7 @@ uint64_t send_r7(uint32_t voltage, uint32_t pattern) {
 	response <<= 8;
 	response |= (crc << 1);
 	response |= 1;
-	uint64_t hz = send_r(response);
+	hz += send_r(response, 48);
 	printf("Sent R7 (%#llx)\n", response);
 	return hz;
 }
@@ -331,19 +415,12 @@ uint64_t send_r7(uint32_t voltage, uint32_t pattern) {
 int main(int argc, char **argv)
 {
 	generateCRCTable();
+	setupCID();
+	setupCSR();
+	setup_io();
 
 	printf("Raspberry Pi SD Card\n");
 	piHiPri(99);
-
-	printf("Tests\n");
-	// CMD0
-	printf("CRC7(0x4000000000) == 0x4a\n");
-	printf("%x\n", crc7(0x4000000000));
-
-	printf("CRC7(0x5100000000) == 0x2a\n");
-	printf("%x\n", crc7(0x5100000000));
-
-	setup_io();
 
 	pinMode(16, INPUT);
 	pinMode(19, OUTPUT);
@@ -355,7 +432,7 @@ int main(int argc, char **argv)
 	pullUpDnControl(16, PUD_DOWN);
 	pullUpDnControl(19, PUD_OFF);
 	pullUpDnControl(20, PUD_OFF);
-	pullUpDnControl(12, PUD_OFF);
+	pullUpDnControl(21, PUD_OFF);
 	pullUpDnControl(26, PUD_UP);
 
 	// clock
@@ -394,39 +471,51 @@ int main(int argc, char **argv)
 				word |= cmd;
 				++bits;
 
-				if(bits == 48) {
-					checkcrc(word);
-
+				if(bits == 48 && checkcrc(word)) {
 					int cmdindex = (word >> 40) & 0x3F;
 
-					if (cmdindex == 0) {
+					// Application commands
+					if (CSR.APP_CMD && cmdindex == 41) {
+						printf("Got ACMD41 (SD_SEND_OP_COND)\n");
+						// Just gonna assume requested features/voltage are ok.
+						CSR.CURRENT_STATE = 1;
+					//	hz += send_r3(1, 1);
+
+						CSR.APP_CMD = 0;
+					} 
+					
+					// Normal commands	
+					else if (cmdindex == 0) {
 						printf("Got CMD0 (GO_IDLE_STATE)\ncds=%d\n", cds);
 						CSR.CURRENT_STATE = 0;
-					} else if(cmdindex == 1) {
+					} else if (cmdindex == 1) {
 						printf("Got CMD1 (SEND_OP_COND)\n");
-						CSR.CURRENT_STATE = 1;
-						hz += send_r3();
+						printf("Don't reply to CMD1, not an MMC card\n");
+					} else if (cmdindex == 2) {
+						printf("Got CMD2 (ALL_SEND_CID)\n");
+					//	hz += send_r2((uint64_t*)&CID);
 					} else if (cmdindex == 8) {
 						printf("Got CMD8 (SEND_IF_COND)\n");
 						uint64_t voltage = (word >> 16) & 0xF;
 						uint64_t pattern = (word >> 8) & 0xFF;
 						printf("voltage = %#llx\n", voltage);
 						printf("pattern = %#llx\n", pattern);
-						if (voltage == 1) {
-							hz += send_r7(voltage, pattern);
-						} else {
-							printf("voltage invalid, not responding\n");
-						}
+						printf("Don't reply to CMD8, only version 1.0 for now\n");
+						//if (voltage == 1) {
+						//	hz += send_r7(voltage, pattern);
+						//} else {
+						//	printf("voltage invalid, not responding\n");
+						//}
 					} else if (cmdindex == 55) {
 						printf("Got CMD55 (APP_CMD)\n");
-						CSR.APP_CMD = 1;
-						hz += send_r1(55);
+					//	hz += send_r1(55);
 					} else {
 						printf("Unknown command!\n");
 						printf("%#llx\n", word);
 						break;
 					}
 
+					pinMode(20, INPUT);
 					start = 0;
 					word = 0;
 					bits = 0;
